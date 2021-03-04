aa444fb8c7d6d3444d23c5eeee0932866470d025
Fray69 <yura.rogovsky2001@gmail.com>
2021-03-04 22:34:44 +0300

    feat: add dream -team func

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {return false;}
+  let sort = members.filter(function (item) {
+    console.log(typeof item);
+    return (typeof item === 'string');
+    });
+
+  return sort.map(function(item) {
+    item = item.trim().toUpperCase();
+    return item = item[0];   
+    }).sort().join('');
+
 };

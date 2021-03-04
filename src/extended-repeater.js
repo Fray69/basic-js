11c2d426a2161da64b693902077352ca2f4a5981
Fray69 <yura.rogovsky2001@gmail.com>
2021-03-04 23:20:47 +0300

    feat: add repit func

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let separ = '+';
+    let defaultaddsepar = '|';
+    let res;
+    if (options.hasOwnProperty('separator')) {separ = options.separator; }
+    if (options.hasOwnProperty('additionSeparator')) { defaultaddsepar = options.additionSeparator; }
+    if (options.hasOwnProperty('addition')) {
+        let addition = options.addition + defaultaddsepar;
+        let fullAddition = Array(options.additionRepeatTimes).fill(addition).join('');
+        fullAddition = fullAddition.slice(0,fullAddition.length - defaultaddsepar.length);
+        res = str + fullAddition + separ;
+    } else { res = str + separ;}   
+    
+    let fulstr = Array(options.repeatTimes).fill(res).join('');
+    fulstr = fullStr.slice(0,fulstr.length - separ.length);
+    console.log(fulstr);
+    return fulstr;
 };
   

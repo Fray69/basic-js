bb0234b3a0660e994aee3a4aceade46b54a3274f
Fray69 <yura.rogovsky2001@gmail.com>
2021-03-04 21:13:13 +0300

    feat: add cats js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  let row = matrix.reduce(function(pritem, item) {
+    return pritem.concat(item);             
+      }, []);  
+    return row.reduce(function(pritem, item){
+      if (item == '^^') {
+          console.log(item);
+          return pritem +=1;
+      } else { return pritem;}
+  }, 0);
 };
